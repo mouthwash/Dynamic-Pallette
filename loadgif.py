2ae36c2e52ffc3a6e50da48182fed4fb40d97ec1
Liszt <lisztlouis@gmail.com>
2016-06-19 08:11:08 -0400

    kill me now

--- loadgif.py
+++ loadgif.py
@@ -1,11 +1,23 @@
 from PIL import Image,GifImagePlugin
 
 #Will load a GIF into its frames as a list of image files
-#Precond-
-#Postcond
+#Precond- accepts a gif image object
+#Postcond - will return a list of image objects with the frames of the gif in it
 def loadgif(gif):
-    gifx = Image.open('day.gif')
-    gifx.show()
+    #create empty gif list
+    giflist = []
+    num = 0
+    #Begin to seek frames and dump them into a
+    #list of images till failure
+    while not EOFError:
+        temp = gif
+        temp = temp.convert('RGB')
+        giflist.append(temp)
+        num = gif.tell() +1
 
-gif = 1
-loadgif(gif)
+    return giflist
+
+#DRIVER PROGRAM TO BE REMOVED LATER
+gif = Image.open('day.gif')
+somelist = loadgif(gif)
+somelist[0].show()
