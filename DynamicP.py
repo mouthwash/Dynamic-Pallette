857fa0ac2a4a44971be675abf25205316bb97e40
marcusDangleben <mad329@cornell.edu>
2016-06-19 00:33:57 -0400

    Completion of base program: color palette displays properly under single image

--- DynamicP.py
+++ DynamicP.py
@@ -4,40 +4,52 @@
 from PIL import Image, ImageDraw
 from Subdivide import subdivide
 from Average import average
+from Selection import selection
 import math
 
+def main():
+    """"""
 
-imName = raw_input('Enter Image Name ')
-im = Image.open(imName)
-base = im.size
+    imName = raw_input('Enter Image Name ')
+    im = Image.open(imName)
+    base = im.size
 
-subs, subsVAL = subdivide(im)
+    subs, subsVAL = subdivide(im)
 
-subdivisions = []
-for sub in subs:
-    subdivisions.append(average(sub))
+    subdivisions = []
+    for sub in subs:
+        subdivisions.append(average(sub))
+
+    subSorted = list(subdivisions)
+    # Alan Zucconi Lumonosity Color Sort
+    # Sort subdivisions based on luminosity
+    def lum(r, g, b):
+        return math.sqrt((.241 * r) + (.691 * g) + (.068 * b))
+    subSorted.sort(key=lambda rgb: lum(*rgb))
+
+    colSelect = selection(subSorted)
+
+    x, y = base
+    a = x / 8
+    b = y / 8
+
+    nIM = Image.new('RGBA', (x + (2 * a), y + (b * 3)), color=(246, 240, 236, 0))
+    nIM.paste(im, (a, b, a + x, b + y))
+
+    draw = ImageDraw.Draw(nIM)
+
+    x0 = a
+    y0 = y + a
+    y1 = y0 + a
+    iter = x / len(colSelect)
+
+    for k in range(len(colSelect)):
+        draw.rectangle([x0, y0, x0 + iter, y1], colSelect[k])
+        x0 += iter
+
+    del draw
+    nIM.show()
 
 
-
-subSorted = list(subdivisions)
-# Alan Zucconi Lumonosity Color Sort
-# Sort subdivisions based on luminosity
-def lum(r, g, b):
-    return math.sqrt((.241 * r) + (.691 * g) + (.068 * b))
-subSorted.sort(key=lambda rgb: lum(*rgb))
-print subSorted
-
-# Liszt 'List' Louis brightness sort method
-# rgb = []
-# for k in subdivisions:
-    # r, g, b = tmp[25, 25]
-    # rgb.append(r + g + b)
-# rgb.sort()
-
-nIM = Image.new('RGBA', base, color=(0, 0, 0, 0))
-draw = ImageDraw.Draw(nIM)
-
-for k in range(len(subs)):
-    draw.rectangle(xy=subsVAL[k], fill=subdivisions[k])
-
-nIM.show()
+if __name__ == '__main__':
+    main()
